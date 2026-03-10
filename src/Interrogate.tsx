8ba5066843e8a8b719f34a0ca3802c54ec2a49db
nand-gate-msn <nandybejoy07@gmail.com>
2026-03-09 21:02:28 -0500

    Minor UI changes, button hover animation, "case file" button animation

--- src/Interrogate.tsx
+++ src/Interrogate.tsx
@@ -83,7 +83,7 @@ function Interrogate() {
             </menu>
           </form>
         </dialog>
-        <button onClick={() => goToBriefing(navigate)}>← Case File</button>
+        <button onClick={() => goToBriefing(navigate)}> <span> ← </span> Case File</button>
         <button onClick={() =>
           (document.getElementById('accuse') as HTMLDialogElement)?.showModal()
         }>Accuse</button>
