7552bfa17c95cb1cba3ef13c732cb03499138a8f
RedWARd42 <Edward6109@gmail.com>
2026-03-10 21:10:58 -0500

    cleaning more code

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import { converse } from './gemini.js';
 import charData from "./data/characters.json" with { type: "json" };
 import fs from 'fs';
 import path from 'path';
@@ -29,29 +28,6 @@ app.use(cors({
 app.use(express.json());
 
 // Defining endpoints
-
-// Gets response from Gemini API, POST method
-app.post('/response', async (req, res) => {
-    const {question, history} = req.body;
-
-    if (!question) {
-        return res.status(400).json({ error: "Missing 'question' in request body" });
-    }
-
-    try {
-        console.log('Received question from frontend:', question);
-        const responseText = await converse(question, history);
-        res.json({ response: responseText });
-    } catch (error) {
-        console.error('Error fetching response:', error);
-        res.status(500).json({ error: 'Internal server error' });
-    }
-});
-
-// Fetches character data from characters.json, GET method
-app.get('/characters', (req, res) => {
-     res.json(charData);
- });
 
  app.post('/save-case', (req, res) => {
      const { caseId, caseTitle, suspects, characterProfiles } = req.body;
