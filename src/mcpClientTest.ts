2ae9ec9b2d04d245012d19823bb4c7e1eed97f8f
RedWARd42 <Edward6109@gmail.com>
2026-03-15 11:12:11 -0500

    Co-authored-by: Nandakishor Bejoy <nand-gate-msn@users.noreply.github.com>

---
+++ src/mcpClientTest.ts
@@ -0,0 +1,10 @@
+import { Client } from "@modelcontextprotocol/sdk/client";
+
+const client = new Client({
+    name: "jsfhh",
+    version: "1.0.0"
+})
+
+async function startClient() {
+    await client.connect("localhost:3000")
+}
