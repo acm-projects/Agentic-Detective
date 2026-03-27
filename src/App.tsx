149d682b3adf603b64d22a2d1ea012ff5c92b773
nand-gate-msn <nandybejoy07@gmail.com>
2026-03-26 20:58:23 -0500

    Restructed Folder Structure

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import CaseReportScreen from './CaseReportScreen.tsx';
 import Interrogate from "./Interrogate";
 import NotesPage from './NotesPage.tsx';
 import Accuse from './Accuse.tsx';
-import CaseResolvedErrorScreen from './components/CaseResolvedErrorScreen.tsx';
+import CaseResolvedErrorScreen from './components/CaseResolvedErrorScreen/CaseResolvedErrorScreen.tsx';
 import { SignIn, SignUp, UserProfile } from '@clerk/react-router';
 import SavedGamesList from './components/savegamelist/SavedGamesList.tsx';
 
